a210b4d8588ba94f25abb6b4b6d90ad08639110c
treble78 <jagr94@list.ru>
2019-01-08 19:28:58 +0300

    Homework-3** Done

---
+++ src/components/Show/Show.js
@@ -0,0 +1,39 @@
+import React,{Component} from 'react'
+import {getShowInfo} from  './api.js'
+
+class Show extends Component {
+    state = {
+        showId:'',
+        data:''
+    }
+
+    componentDidUpdate(prevProps,prevState){
+        console.log(prevProps,prevState);
+        console.log(this.props,this.state);
+        if (prevProps.showId !== this.props.showId){
+           getShowInfo(this.props.showId).then(film=>this.setState({data:film}));
+        }      
+    }
+
+    render(){
+        const {showID,data} = this.state;
+        return  (
+        <div className="show">
+            <img className="show-image" 
+            src={data === '' ? '' : data.image.medium}/>
+            <h2 className="show-labe t-show-name">
+            {data === '' ? '' : data.name}
+            </h2>
+            <p className="show-text t-show-genre">
+            <b>Жанр:</b>
+            {data === '' ? '' : data.genres.join(',')}
+            </p>
+            <p className="show-text t-show-summary">
+            {data === '' ? '' : data.summary}
+            </p>
+        </div>  
+        );
+    }
+}
+
+export default Show;
