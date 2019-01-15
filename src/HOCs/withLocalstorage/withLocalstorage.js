52a325c33a4f89049b172bf4e5c23577893359be
treble78 <jagr94@list.ru>
2019-01-16 00:45:20 +0300

    Homework-5 Done

--- src/HOCs/withLocalstorage/withLocalstorage.js
+++ src/HOCs/withLocalstorage/withLocalstorage.js
@@ -1,6 +1,47 @@
 import React, { Component } from 'react';
 import { load, save } from '../../localstorage';
 
-const withLocalstorage = () => () => {};
+const withLocalstorage = (storageKey, notasks) => (WrappedComponent)  => {
+    return class extends Component{
+        state = {
+            savedData: notasks
+        }
+
+        saveData = (task) => {
+
+            const {savedData} = this.state;
+            let dataArr = [];
+
+            const isOldRecord = (record) => record.id === task.id
+
+            if(!savedData){
+                dataArr = [task]
+            } 
+            else if( savedData.findIndex(isOldRecord) !== -1) {
+                dataArr = savedData.map(item => isOldRecord(item) ? task : item)
+            } 
+            else { dataArr = [...savedData, task] }
+
+            save(storageKey, dataArr);
+            this.setState({savedData: load(storageKey)})
+        }
+
+        componentDidMount() {
+            this.setState({savedData: load(storageKey)})
+        }
+
+        render(){
+            
+            return (
+                <WrappedComponent 
+                    savedData={this.state.savedData} 
+                    saveData={this.saveData} 
+                    {...this.props}
+                >
+                </WrappedComponent>
+            )
+        }
+    }
+};
 
 export default withLocalstorage;
--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -4,20 +4,98 @@ import './Todo.css';
 import withLocalstorage from '../../HOCs/withLocalstorage';
 
 class Todo extends PureComponent {
+  
   state = {
     inputValue: ''
   };
 
-  handleChange = event => {};
-  createNewRecordByEnter = event => {};
-  toggleRecordComplete = event => {};
-  createNewRecord = () => {};
-  renderEmptyRecord = () => {};
-  renderRecord = () => {};
+  getId() {
+    const { savedData } = this.props;
+    if(!savedData) return 1;
+    const biggest = savedData.reduce((acc, el) => Math.max(acc, el.id), 0);
+    return biggest + 1;
+  }
 
- render(){
-   return false;
- }
+  handleChange = (e) => {
+    this.setState({inputValue: e.target.value})
+  };
+
+  createNewTaskByEnter = (e) => {
+    e.key === 'Enter' && this.createNewTask()
+  };
+
+  toggleTaskStatus = (task) => {
+    const {saveData} = this.props;
+    return () => {
+      const updatedTask = {
+        id: task.id,
+        isComplete: !task.isComplete,
+        text: task.text
+      };
+      saveData(updatedTask);
+    }
+  };
+
+  createNewTask = () => {
+    const {saveData} = this.props;
+    const {inputValue} = this.state;
+    if(inputValue) {
+        const newTask = {
+        id: this.getId(),
+        isComplete: false,
+        text: inputValue
+      };
+      
+      this.setState({ inputValue: '' });
+      saveData(newTask);
+    }
+    
+  };
+
+  renderEmptyTask = () => {
+    return (
+      <div className='todo-item t-todo'>Задач нет</div>
+    )
+  }
+
+  renderTask = task => {
+    return (
+      <div className='todo-item t-todo' key={task.id}>
+        <p className='todo-item__text'>{task.text}</p>
+        <span 
+          className='todo-item__flag t-todo-complete-flag'
+          data-todo-id={task.id}
+          onClick={this.toggleTaskStatus(task)}
+        >
+          {`[${task.isComplete ? 'x' : ' '}]`}
+        </span>
+      </div>
+    )
+  };
+
+  render() {
+    const {savedData} = this.props;
+    const {inputValue} = this.state;
+    return (
+      <Card title='Список дел'>
+        <div className='todo t-todo-list'>
+          <div className='todo-item todo-item-new'>
+            <input 
+              className='todo-input t-input' 
+              placeholder='Введите задачу' 
+              onChange={this.handleChange}
+              onKeyPress={this.createNewTaskByEnter}
+              value={inputValue}
+            />
+            <span className='plus t-plus' onClick={this.createNewTask}>+</span>
+          </div>
+          {savedData ? 
+            savedData.map((record) => this.renderTask(record) ) : 
+            this.renderEmptyTask()}
+        </div>
+      </Card>
+    )
+  }
 }
 
 export default withLocalstorage('todo-app', [])(Todo);
